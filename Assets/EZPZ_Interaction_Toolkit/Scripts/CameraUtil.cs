5645308a1b96673d779636fc4e0a0ec7445a16ad
AVataRR626 <matt.cabanag@gmail.com>
2023-04-14 15:16:50 +1000

    Virtual Phone Project

---
+++ Assets/EZPZ_Interaction_Toolkit/Scripts/CameraUtil.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraUtil : MonoBehaviour
+{
+    public Camera subject;
+    public float outputRatio = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (subject == null)
+            subject = GetComponent<Camera>();
+        subject.aspect = outputRatio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        subject.aspect = outputRatio;
+    }
+}
