97a0415631cc0b51578665958140c76e32c52320
AVataRR626 <matt.cabanag@gmail.com>
2023-09-27 11:26:47 +1000

    general kenobi
    hello there

--- Assets/EZPZ_Interaction_Toolkit/Scripts/TextMatchRelay.cs
+++ Assets/EZPZ_Interaction_Toolkit/Scripts/TextMatchRelay.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class TextMatchRelay : MonoBehaviour
 {    
     public List<TextMatchTrigger> relayList;
+    public bool autoPopulateChildren = true;
+
+    private void Start()
+    {
+        if(autoPopulateChildren)
+        {
+            foreach(Transform child in transform)
+            {
+                TextMatchTrigger tmt = child.GetComponent<TextMatchTrigger>();
+
+                if (tmt != null)
+                    relayList.Add(tmt);
+            }
+        }
+    }
 
     public void CheckMatch()
     {
