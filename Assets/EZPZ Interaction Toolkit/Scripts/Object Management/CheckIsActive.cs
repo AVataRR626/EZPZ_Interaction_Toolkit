77f1a513dbc318d479fca42f9393861eca5e4884
AVataRR626 <matt.cabanag@gmail.com>
2025-10-12 17:59:26 +1100

    counting multiple choice incorrect/correct answers

--- Assets/EZPZ Interaction Toolkit/Scripts/Object Management/CheckIsActive.cs
+++ Assets/EZPZ Interaction Toolkit/Scripts/Object Management/CheckIsActive.cs
@@ -1,3 +1,7 @@
+//CheckIsActive -
+//Event rig for checking if the subject object is active or not.
+//by Matt Cabanag, Created On: Don't remember when!
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
